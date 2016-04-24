716b548612ef6a0568eee4cb58cf5db32db22626
Joshua Partogi <proscrumtrainer@live.com>
2016-04-25 01:42:42 +0800

    Clean up code

--- src/main/java/yose/YoseGame.java
+++ src/main/java/yose/YoseGame.java
@@ -10,19 +10,9 @@ import static spark.Spark.*;
 
 public class YoseGame {
     public static void main(String[] args) {
-    	port(getHerokuAssignedPort());
-    	
     	get("/", (request, response) -> {
 
             return new ModelAndView(new Object(), "index.ftl");
         }, new FreeMarkerEngine());
-    }
-    
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
